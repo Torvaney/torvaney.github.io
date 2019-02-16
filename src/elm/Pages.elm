fe432ddc403cbf19cca425cad352ff5cbc4cf4e2
Torvaney <torvaney@protonmail.com>
2019-02-16 13:48:01 +0000

    Update layout (again)

--- src/elm/Pages.elm
+++ src/elm/Pages.elm
@@ -14,16 +14,21 @@ Email: torvaney [at] protonmail.com
 projectText = """
 Some links:
 
-* Soccer
+### Soccer
+
   * [xG interactive](projects/xG.html)
   * [Soccer event logger](projects/tracker.html)
   * [XGEEFAX](https://twitter.com/XGEEFAX)
   * [FPL optimiser twitter bot](https://twitter.com/fpl_optimiser)
   * [blog](http://www.statsandsnakeoil.com)
-* NLP (kinda)
+
+### NLP (kinda)
+
   * [Test your (human) language model](projects/human-language-model.html)
   * [The Duck Debugger](projects/duck-debugger.html)
-* Miscellaneous
+
+### Miscellaneous
+
   * [Chaos game](projects/chaosgame.html)
   * [Messenger and army interactive demo](projects/messenger-army-puzzle.html)
   * [Wolf and Hare](projects/wolf-and-hare.html)
